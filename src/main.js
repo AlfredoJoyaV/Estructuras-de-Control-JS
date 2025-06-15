f59e7a238c47bf49510c4f1298a0416547ca0fb0
Erick Joya <ejoyavaz@gmail.com>
2025-06-15 10:04:30 -0600

    :white_check_mark:update:Se agrego el ejercicio 4 y se actualizo los demas ejercicios con una nueva funcionalidad

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ const app = document.getElementById('app')
 //Hacemos el ejercicio 1
 const ejercicio1Content = `
 <div class="ejercicio1">
-  <h1 id="titulo1">Ejercicio 1:</h1>
+  <h1 id="titulo1">EJERCICIO 1:</h1>
   <p> Crear una función que en base a la edad que ingreso el usuario devolver un mensaje si la persona es mayor de edad o no. Utilizar para la condición el operador ternario</p>
   <input type="number" id="edad" placeholder="Ingrese tu edad">
   <button id="btn-ejercicio1">Calcular</button>
@@ -24,6 +24,12 @@ btnEjercicio1.addEventListener('click', () => {
   const edad = parseInt(document.getElementById('edad').value);
   const resultado = document.getElementById('resultado-ejercicio1');
 
+ // Validamos que los campos no esten vacios
+  if (document.getElementById('edad').value === "") {
+    document.getElementById('resultado-ejercicio1').textContent = "Por favor, ingresa tu edad.";
+    return;
+  }
+
   // Usar el operador ternario para determinar si es mayor de edad
   const mensaje = (edad >= 18) ? 'Tu eres mayor de edad.' : 'Tu eres menor de edad.';
   resultado.textContent = mensaje;
@@ -32,7 +38,7 @@ btnEjercicio1.addEventListener('click', () => {
 //Hacemos el ejercicio 2
 const ejercicio2Content = `
 <div class="ejercicio2">
-  <h1 id="titulo2">Ejercicio 2:</h1>
+  <h1 id="titulo2">EJERCICIO 2:</h1>
   <p>Crear una función que determine la nota final de un alumno, la cual depende de lo siguiente:</p>
   <ul>
     <li>Examen = 20%</li>
@@ -75,6 +81,12 @@ btnEjercicio2.addEventListener('click', () => {
   const notaAsistencia = parseFloat(document.getElementById('notaAsistencia').value);
   const notaInvestigacion = parseFloat(document.getElementById('notaInvestigacion').value);
 
+  // Validamos que los campos no esten vacios
+  if (document.getElementById('nombre').value === "" || document.getElementById('carnet').value === "" || isNaN(notaExamen) || isNaN(notaTareas) || isNaN(notaAsistencia) || isNaN(notaInvestigacion)) {
+    document.getElementById('resultado-ejercicio2').textContent = "Por favor, completa todos los campos.";
+    return;
+  }
+
   // Calcular la nota final
   const notaFinal = (notaExamen * 0.2) + (notaTareas * 0.4) + (notaAsistencia * 0.1) + (notaInvestigacion * 0.3);
 
@@ -88,7 +100,7 @@ btnEjercicio2.addEventListener('click', () => {
 //Hacemos el ejercicio 3
 const ejercicio3Content = `
 <div class="ejercicio3">
-  <h1 id="titulo3">Ejercicio 3:</h1>
+  <h1 id="titulo3">EJERCICIO 3:</h1>
   <p>Calcular el aumento de trabajador tomando en cuenta la tabla de categorías de aumento.
   Para este ejercicio deberá de asignar las siguientes variables: nombre, salario, categoría y aumento. 
   Deberá demostrar los datos del empleado y el aumento salarial.
@@ -123,6 +135,12 @@ btnEjercicio3.addEventListener('click', () => {
   const salarioActual = parseFloat(document.getElementById('salarioActual').value);
   const categoriaAumento = document.getElementById('categoriaAumento').value.toUpperCase();
 
+  // Validamos que los campos no esten vacios
+  if (document.getElementById('nombreEmpleado').value === "" || document.getElementById('salarioActual').value === "" || document.getElementById('categoriaAumento').value === "") {
+    document.getElementById('resultado-ejercicio3').textContent = "Por favor, completa todos los campos.";
+    return;
+  }
+
   let aumento = 0;
 
   // Determinar el aumento
@@ -152,3 +170,47 @@ btnEjercicio3.addEventListener('click', () => {
   document.getElementById('resultadoAumento').textContent = `Aumento: $${aumento.toFixed(2)}`;
   document.getElementById('resultadoSalarioFinal').textContent = `Salario Final: $${salarioFinal.toFixed(2)}`;
 });
+
+//Hacemos el ejercicio 4
+const ejercicio4Content = `
+<div class="ejercicio4">
+  <h1 id="titulo4">EJERCICIO 4:</h1>
+  <p>Crear una función que en base a 2 números enteros que ingrese el usuario, calcular cual número es el mayor y devolverlo.</p>
+  <input type="number" id="numero1" step="1" placeholder="Ingrese el primer número">
+  <input type="number" id="numero2" step="1" placeholder="Ingrese el segundo número">
+  <button id="btn-ejercicio4">Mostrar el numero mayor</button>
+  <p id="resultado-ejercicio4"></p>
+  <div class="resultado-numeros">
+    <p id="resultadoNumeroMayor"></p>
+</div>
+`;
+
+// Crear el contenedor del ejercicio 4 y agregarlo al DOM
+const ejercicio4Container = document.createElement('div');
+ejercicio4Container.innerHTML = ejercicio4Content;
+app.appendChild(ejercicio4Container);
+// Agregar el evento al botón del ejercicio 4
+const btnEjercicio4 = document.getElementById('btn-ejercicio4');
+btnEjercicio4.addEventListener('click', () => {
+  const numero1 = parseInt(document.getElementById('numero1').value);
+  const numero2 = parseInt(document.getElementById('numero2').value);
+
+    // Validamos que los campos no esten vacios
+  if (document.getElementById('numero1').value === "" || document.getElementById('numero2').value === "") {
+    document.getElementById('resultado-ejercicio4').textContent = "Por favor, ingresa ambos números.";
+    return;
+  }
+
+   // Validamos que sean numero enteros
+  if (!Number.isInteger(numero1) || !Number.isInteger(numero2)) {
+    document.getElementById('resultado-ejercicio4').textContent = "Solo se permiten números enteros";
+    return;
+  }
+
+  // Determinar el número mayor
+  const numeroMayor = (numero1 > numero2) ? numero1 : numero2;
+
+  // Mostramos el resultado
+  document.getElementById('resultadoNumeroMayor').textContent = `El número mayor es: ${numeroMayor}`;
+});
+
